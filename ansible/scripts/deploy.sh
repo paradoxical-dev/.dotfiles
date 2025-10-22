f69445cc73c70d686142ea034d4410d4dd3c0fc6
paradoxical-dev <jaredonnell21@gmail.com>
2025-10-21 23:53:02 -0500

    feat(automation): ansible introduction
    Added ansible automation to the mix. Plan on using it over the script
    centric flow or the other tool I was making (TheForge)

---
+++ ansible/scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#==================================================================#
+#  Script will kick off the ansible post installation autmoation.  #
+#  Checks for the base packages (ansible, gum) and proompts user   #
+#     for device/profile type before starting installation.        #
+#==================================================================#
+
+set -e
+
+pkg_exists() {
+    qlist -I "$1" &> /dev/null || command -v "$1" &> /dev/null
+}
+
+yellow="\e[1;33m"
+color_end="\e[1;0m"
+
+if ! pkg_exists "ansible"; then
+    echo -e "${yellow}ansible not yet installed.\n${color_end}"
+    echo -e "${yellow}Installing ansible now...${color_end}"
+    sudo emerge --oneshot --verbose "app-admin/ansible"
+fi
+
+# TODO:  will need to be changed once scripts dir is mergerd to main repo
+ANSIBLE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+cd "$ANSIBLE_DIR"
+
+ansible-playbook test.yml
